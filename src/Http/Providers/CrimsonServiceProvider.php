5712307bdcde2d8cf2194cc5278d3a7ef615a9fe
KingMaj0r <abdurrahman@barakat.email>
2024-10-11 15:18:07 +0700

    Added CrimsonServiceProvider

---
+++ src/Http/Providers/CrimsonServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PulseFrame\Crimson\Http\Providers;
+
+use PulseFrame\Facades\Config;
+use PulseFrame\Contracts\ServiceProviderInterface;
+use PulseFrame\Http\Handlers\RouteHandler;
+use PulseFrame\Crimson\Http\Handlers\ExceptionHandler;
+
+class CrimsonServiceProvider implements ServiceProviderInterface
+{
+  public function register(): void
+  {
+    Config::set('view', "error_handler", [self::class, 'handleCrimsonErrors']);
+
+    RouteHandler::setErrorHandler([ExceptionHandler::class, 'handle']);
+  }
+
+  public function boot(): void {}
+}
